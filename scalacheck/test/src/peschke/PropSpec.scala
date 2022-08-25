5a399f9be7a2b780d324147c043535c604ee0b48
Morgen Peschke <morgen.peschke@gmail.com>
2022-08-25 15:53:40 -0700

    Cleanup unused test trait

---
+++ scalacheck/test/src/peschke/PropSpec.scala
@@ -0,0 +1,19 @@
+package peschke
+
+import org.scalacheck.Shrink
+import org.scalatest.EitherValues
+import org.scalatest.OptionValues
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.propspec.AnyPropSpec
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+trait PropSpec
+    extends AnyPropSpec
+    with Matchers
+    with EitherValues
+    with OptionValues
+    with ScalaCheckDrivenPropertyChecks
+    with TableDrivenPropertyChecks {
+  implicit def noShrink[T]: Shrink[T] = Shrink.shrinkAny
+}
